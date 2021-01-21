8949b218c9e35279d8624cdeda84b35c7a2a1751
maiqm <maiqiming@ggn.com>
2021-01-21 18:06:34 +0800

    新增 dynamic、var、Object三者的区别 小知识

--- flutter_dart_learn/lib/data_type.dart
+++ flutter_dart_learn/lib/data_type.dart
@@ -13,6 +13,7 @@ class _DataTypeState extends State<DataType> {
     _boolType();
     _listType();
     _mapType();
+    _tips();
     return Container(
       child: Text('常用数据类型,请看控制台输出'),
     );
@@ -219,5 +220,31 @@ class _DataTypeState extends State<DataType> {
       print(' for方式   key:  $key   value:   ${ages[key]}');
     }
 
+  }
+
+  ///科普小知识：dynamic、var、Object三者的区别
+  ///dynamic：是所有Dart对象的基础类型， 在大多数情况下，通常不直接使用它，
+  ///通过它定义的变量会关闭类型检查，这意味着 dynamic x = 'hal';x.foo();
+  ///这段代码静态类型检查不会报错，但是运行时会crash，因为x并没有foo()方法，所以建议大家在编程时不要直接使用dynamic；
+  ///var：是一个关键字，意思是“我不关心这里的类型是什么。”，系统会自动推断类型runtimeType；
+  ///Object：是Dart对象的基类，当你定义：Object o=xxx；时这时候系统会认为o是个对象，你可以调用o的toString()和hashCode()方法
+  ///因为Object提供了这些方法，但是如果你尝试调用o.foo()时，静态类型检查会进行报错；
+  ///综上不难看出dynamic与Object的最大的区别是在静态类型检查上；
+  void _tips() {
+    print('------------------tips---------------------------');
+
+    dynamic x = 'hal';
+    print(x.runtimeType);
+    print(x);
+    x = 123;
+    print(x);
+    print(x.runtimeType);
+    var a = 'hal';
+    //a = 123;//你可以改变 x的类型，但是无法改变 a的类型
+    print(a.runtimeType);
+    print(a);
+    Object o1 = '11';
+    print(o1.runtimeType);
+    print(o1);
   }
 }
